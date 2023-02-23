0eb4925db366ce262e0270d6676b39d29e73681a
Baptiste Lombard <btlcasio@gmail.com>
2023-02-23 15:36:53 +0100

    ♻️ Remove unused

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
-import GitHubProject from "github-project";
-
-const project = new GitHubProject({
-  owner: "Leexi-ai",
-  number: 1,
-  token: "ghp_...",
-  fields: {
-    iteration: "Cycle",
-  },
-});
-
-const statuses = ['Done', 'Todo', 'In Progress', 'In Review'];
-
-const projectData = await project.getProperties();
-// console.log(projectData.fields.iteration.configuration);
-
-const lastIteration = projectData.fields.iteration.configuration.completedIterations[0];
-const currentIteration = projectData.fields.iteration.configuration.iterations[0];
-const nextIteration = projectData.fields.iteration.configuration.iterations[1];
-
-const iteration = lastIteration;
-// const newIteration = newiterationType === 'current' ? currentIteration : nextIteration;
-
-const items = await project.items.list();
-const filteredItems = items.filter(item => statuses.includes(item.fields.status) && item.fields.iteration === iteration.title);
-console.log(filteredItems);
+const GitHubProject = require("github-project");
 
 const run = async () => {
   try {
@@ -52,15 +27,3 @@ const run = async () => {
     core.setFailed(error.message);
   }
 };
-
-// if (require.main === module) {
-  // run();
-// }
-
-
-// node_modules/github-project/api/lib/project-node-to-properties.js
-//   fields: state.fields,
-// node_modules/github-project/api/lib/project-fields-nodes-to-fields-map.js
-//   acc[userInternalFieldName].configuration = node.configuration;
-// node_modules/github-project/index.d.ts
-//   fields: ProjectFieldMap;
